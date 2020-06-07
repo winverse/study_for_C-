3e0de64ad30144a9263f871680a6490fc680b5f8
winverse <public.winverse@gmail.com>
2020-06-07 23:41:26 +0900

    DisplayContants

---
+++ 01/DisplayContants/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DisplayContants // constants = magic Number 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine(30);
+           Console.WriteLine(30.1); // double
+           Console.WriteLine(30.2f); // float
+           Console.Write(true); // why is it printed in capital letters?
+           Console.WriteLine('p');
+           Console.WriteLine(10 / 20); // why it is return value zero?
+        }
+    }
+}
