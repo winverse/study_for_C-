1524f241bfea5e7e52e323cdd47ef42369048d55
winverse <public.winverse@gmail.com>
2020-06-10 01:23:20 +0900

    Variables

--- 01/DisplayContants/Program.cs
+++ 01/DisplayContants/Program.cs
@@ -6,12 +6,12 @@ namespace DisplayContants // constants = magic Number
     {
         static void Main(string[] args)
         {
-           Console.WriteLine(30);
-           Console.WriteLine(30.1); // double
-           Console.WriteLine(30.2f); // float
-           Console.Write(true); // why is it printed in capital letters?
-           Console.WriteLine('p');
-           Console.WriteLine(10 / 20); // why it is return value zero?
+            Console.WriteLine(30);
+            Console.WriteLine(30.1); // double
+            Console.WriteLine(30.2f); // float
+            Console.Write(true); // why is it printed in capital letters?
+            Console.WriteLine('p');
+            Console.WriteLine(10 / 20); // why it is return value zero?
         }
     }
-}
+} 
